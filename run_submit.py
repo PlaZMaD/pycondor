958ecf7e16f9d487e139a8731a4588a3eacb46b2
Evgeniy Kurbatov <lelituser@gmail.com>
2024-07-02 00:11:18 +0300

    env setup update

--- run_submit.py
+++ run_submit.py
@@ -16,6 +16,9 @@ print(input_files_db)
 credd = htcondor.Credd()
 credd.add_user_cred(htcondor.CredTypes.Kerberos, None)
 
+
+os.remove(os.path.join(log_dir,"*"))
+
 for fileN, path in enumerate(input_files_db['path'].to_list()):
 
     
@@ -29,7 +32,7 @@ for fileN, path in enumerate(input_files_db['path'].to_list()):
         "log": os.path.join(log_dir, f"cat-{fileN}-$(ProcId).log"),              
         "request_cpus": "1",
         'MY.SendCredential': True,
-        'environment' :f"EOS_DATA={output_dir} inputFile={path}"#SHIP_CVMFS_SETUP_FILE=$ENV(SHIP_CVMFS_SETUP_FILE) FAIRSHIP_DIR=$ENV(FAIRSHIP) MAGNET_GEO=$ENV(MAGNET_GEO)"
+        'environment' :f'"EOS_DATA={output_dir} inputFile={path}"'#SHIP_CVMFS_SETUP_FILE=$ENV(SHIP_CVMFS_SETUP_FILE) FAIRSHIP_DIR=$ENV(FAIRSHIP) MAGNET_GEO=$ENV(MAGNET_GEO)"
              
     #    "request_memory": "4Gi",       
     #    "request_disk": "1Gi",           
