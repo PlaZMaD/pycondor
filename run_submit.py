95f5b9d23eb58f01b32da529478ae3eff607597d
PlaZMaD <lelituser@gmail.com>
2024-07-02 14:05:14 +0300

    Update run_submit.py
    new start fs args logic

--- run_submit.py
+++ run_submit.py
@@ -3,8 +3,16 @@ import classad
 import pandas as pd
 import os
 import glob
+import numpy as np
 
 run_tag = "first_run"
+debug = True
+extra_fs_args = " --fastMuon --MuonBack"
+
+events_per_job = 250000
+
+
+
 output_dir = '/eos/experiment/ship/user/ekurbato/condor_output/'
 log_dir = "/afs/cern.ch/work/e/ekurbato/public/condor_logs/"
 subdir = 'test_run'
@@ -22,12 +30,15 @@ files = glob.glob(os.path.join(log_dir, '*'))
 for f in files:
     os.remove(f)
 
-for fileN, path in enumerate(input_files_db['path'].to_list()):
+for _, row in df.iterrows():#fileN, path in enumerate(input_files_db['path'].to_list()):
 
-    
+    lPath = row['path']
+    total_events = row['nEvents']
+    fid = row['id']
+
     job_template = {
         "executable": "test.sh",      
-        "arguments": "$(input_file_name)",          # we will pass in the value for this macro via itemdata
+        "arguments": "$(input_file_name) $(start_event) $(nEvents) $(extra_fs_args)",          # we will pass in the value for this macro via itemdata
         "transfer_input_files": "$(input_file)",    # we also need HTCondor to move the file to the execute node
         "should_transfer_files": "yes",             # force HTCondor to transfer files even though we're running entirely inside a container (and it normally wouldn't need to)
         "output": os.path.join(log_dir, f"fs-{fileN}-$(ProcId).out"),  
@@ -35,18 +46,30 @@ for fileN, path in enumerate(input_files_db['path'].to_list()):
         "log": os.path.join(log_dir, f"cat-{fileN}-$(ProcId).log"),              
         "request_cpus": "1",
         'MY.SendCredential': True,
-        'environment' :f'"out_dir={os.path.join(output_dir, run_tag, str(fileN))}/$(subjob) inputFile={path}"'#SHIP_CVMFS_SETUP_FILE=$ENV(SHIP_CVMFS_SETUP_FILE) FAIRSHIP_DIR=$ENV(FAIRSHIP) MAGNET_GEO=$ENV(MAGNET_GEO)"
+        'environment' :f'"out_dir={os.path.join(output_dir, run_tag, str(fid))}/$(subjob)"'# inputFile={lPath}"'#SHIP_CVMFS_SETUP_FILE=$ENV(SHIP_CVMFS_SETUP_FILE) FAIRSHIP_DIR=$ENV(FAIRSHIP) MAGNET_GEO=$ENV(MAGNET_GEO)"
              
     #    "request_memory": "4Gi",       
     #    "request_disk": "1Gi",           
     }
 
-    iter_data = [{'input_file_name': str(path), 'subjob': str(subjob)} for subjob, path in enumerate(range(2))]
+    iter_data = []#[{'input_file_name': str(path), 'subjob': str(subjob)} for subjob, path in enumerate(range(2))]
+    for i in range(np.ceil(total_events / events_per_job)):
+        start_event = i * events_per_job
+        nEvents = events_per_job if (start_event + events_per_job) <= total_events else (total_events - start_event)
+        iter_data.append({'input_file_name':lPath, 'subjob': str(i), 'start_event': str(start_event), 'nEvents':str(nEvents), 'extra_fs_args': extra_fs_args})
+
+    if debug:
+        iter_data = iter_data[:2]
+        iter_data = [i.update({'nEvents':10}) for i in iter_data]
+
     schedd = htcondor.Schedd()
     cat_job = htcondor.Submit(job_template)
     submit_result = schedd.submit(cat_job, itemdata = iter(iter_data))  # submit one job for each item in the itemdata
 
     print(submit_result.cluster())
+    
+    if debug:
+        break
 
 def run_connmon(files, jobs_per_file=50, output=None):
     pass
