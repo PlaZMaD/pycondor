84932aa7d120c63fde24482f6e757310489948ea
PlaZMaD <lelituser@gmail.com>
2024-07-02 14:08:10 +0300

    Update run_submit.py
    fix
    
    Update run_submit.py
    
    Update run_submit.py
    
    fix
    
    Update run_submit.py
    
    fix
    
    Update run_submit.py
    
    fix
    
    Update run_submit.py
    
    fix
    
    Update run_submit.py
    
    fix
    
    Update run_submit.py
    
    fix

--- run_submit.py
+++ run_submit.py
@@ -7,7 +7,7 @@ import numpy as np
 
 run_tag = "first_run"
 debug = True
-extra_fs_args = " --fastMuon --MuonBack"
+extra_fs_args = r'\" --fastMuon --MuonBack\"'
 
 events_per_job = 250000
 
@@ -30,7 +30,7 @@ files = glob.glob(os.path.join(log_dir, '*'))
 for f in files:
     os.remove(f)
 
-for _, row in df.iterrows():#fileN, path in enumerate(input_files_db['path'].to_list()):
+for _, row in input_files_db.iterrows():#fileN, path in enumerate(input_files_db['path'].to_list()):
 
     lPath = row['path']
     total_events = row['nEvents']
@@ -41,9 +41,9 @@ for _, row in df.iterrows():#fileN, path in enumerate(input_files_db['path'].to_
         "arguments": "$(input_file_name) $(start_event) $(nEvents) $(extra_fs_args)",          # we will pass in the value for this macro via itemdata
         "transfer_input_files": "$(input_file)",    # we also need HTCondor to move the file to the execute node
         "should_transfer_files": "yes",             # force HTCondor to transfer files even though we're running entirely inside a container (and it normally wouldn't need to)
-        "output": os.path.join(log_dir, f"fs-{fileN}-$(ProcId).out"),  
-        "error": os.path.join(log_dir, f"fs-{fileN}-$(ProcId).err"),  
-        "log": os.path.join(log_dir, f"cat-{fileN}-$(ProcId).log"),              
+        "output": os.path.join(log_dir, f"fs-{fid}-$(ProcId).out"),  
+        "error": os.path.join(log_dir, f"fs-{fid}-$(ProcId).err"),  
+        "log": os.path.join(log_dir, f"cat-{fid}-$(ProcId).log"),              
         "request_cpus": "1",
         'MY.SendCredential': True,
         'environment' :f'"out_dir={os.path.join(output_dir, run_tag, str(fid))}/$(subjob)"'# inputFile={lPath}"'#SHIP_CVMFS_SETUP_FILE=$ENV(SHIP_CVMFS_SETUP_FILE) FAIRSHIP_DIR=$ENV(FAIRSHIP) MAGNET_GEO=$ENV(MAGNET_GEO)"
@@ -53,21 +53,23 @@ for _, row in df.iterrows():#fileN, path in enumerate(input_files_db['path'].to_
     }
 
     iter_data = []#[{'input_file_name': str(path), 'subjob': str(subjob)} for subjob, path in enumerate(range(2))]
-    for i in range(np.ceil(total_events / events_per_job)):
+    for i in range(np.ceil(total_events / events_per_job).astype('int')):
         start_event = i * events_per_job
         nEvents = events_per_job if (start_event + events_per_job) <= total_events else (total_events - start_event)
         iter_data.append({'input_file_name':lPath, 'subjob': str(i), 'start_event': str(start_event), 'nEvents':str(nEvents), 'extra_fs_args': extra_fs_args})
 
     if debug:
         iter_data = iter_data[:2]
-        iter_data = [i.update({'nEvents':10}) for i in iter_data]
+        iter_data[0]['nEvents'] = '10'
+        iter_data[1]['nEvents'] = '10'
 
     schedd = htcondor.Schedd()
     cat_job = htcondor.Submit(job_template)
+    # print(iter_data)
     submit_result = schedd.submit(cat_job, itemdata = iter(iter_data))  # submit one job for each item in the itemdata
 
     print(submit_result.cluster())
-    
+
     if debug:
         break
 
