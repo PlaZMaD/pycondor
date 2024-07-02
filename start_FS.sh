89b5512e212f380ed35fa2026611a9e5b302a129
Evgeniy Kurbatov <lelituser@gmail.com>
2024-07-02 19:42:50 +0300

    mkdir add

--- start_FS.sh
+++ start_FS.sh
@@ -2,10 +2,9 @@
 cd $1
 source /afs/cern.ch/work/e/ekurbato/public/fs_setups/master/setUp.sh
 eval $(alienv -a slc9_x86-64 load FairShip/latest-master-release --no-refresh)
-# source start_ali.sh
 cd -
 set -ux
 echo "Starting script."
 
-
+mkdir -p $out_dir
 python $FAIRSHIP/macro/run_simScript.py $5 --nEvents $4 --firstEvent $3 -f $2 --output $out_dir
