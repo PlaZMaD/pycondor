caec20defb9bfb93bc3050693247f27820c12584
Evgeniy Kurbatov <lelituser@gmail.com>
2024-07-03 17:23:41 +0300

    fix

--- start_FS.sh
+++ start_FS.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#cd $2
 source $1
 eval $(alienv -w $2/sw -a slc9_x86-64 load FairShip/latest-master-release --no-refresh)
-#cd -
 set -ux
 echo "Starting script."
-# temp=`echo $5 | tr -d '\''`
 mkdir -p $out_dir
 python $FAIRSHIP/macro/run_simScript.py ${@:6} --nEvents $5 --firstEvent $4 -f $3
-#--output $out_dir
 cp *.root $out_dir 
