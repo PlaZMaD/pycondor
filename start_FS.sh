6d16fc0142374d708fa02fdc364c8d6db59cb159
Evgeniy Kurbatov <lelituser@gmail.com>
2024-07-03 14:37:45 +0300

    fix

--- start_FS.sh
+++ start_FS.sh
@@ -7,5 +7,6 @@ set -ux
 echo "Starting script."
 # temp=`echo $5 | tr -d '\''`
 mkdir -p $out_dir
-python $FAIRSHIP/macro/run_simScript.py ${@:6} --nEvents $5 --firstEvent $4 -f $3 #--output $out_dir
+python $FAIRSHIP/macro/run_simScript.py ${@:6} --nEvents $5 --firstEvent $4 -f $3
+#--output $out_dir
 cp *.root $out_dir 
