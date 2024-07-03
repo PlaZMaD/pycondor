5ac2e8ff421797e2952056b4b84682ed628f4f56
Evgeniy Kurbatov <lelituser@gmail.com>
2024-07-03 14:19:31 +0300

    new output logic

--- start_FS.sh
+++ start_FS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd $2
+#cd $2
 source $1
-eval $(alienv -a slc9_x86-64 load FairShip/latest-master-release --no-refresh)
-cd -
+eval $(alienv -a -w $2/sw slc9_x86-64 load FairShip/latest-master-release --no-refresh)
+#cd -
 set -ux
 echo "Starting script."
 # temp=`echo $5 | tr -d '\''`
 mkdir -p $out_dir
-python $FAIRSHIP/macro/run_simScript.py ${@:6} --nEvents $5 --firstEvent $4 -f $3 --output $out_dir
+python $FAIRSHIP/macro/run_simScript.py ${@:6} --nEvents $5 --firstEvent $4 -f $3 #--output $out_dir
+cp *.root $out_dir 
